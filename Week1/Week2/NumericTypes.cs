12449481313c2884faac212b7f87352b171430c0
paxkharybdis <35146073+paxkharybdis@users.noreply.github.com>
2018-08-29 19:40:04 -0500

    Correction

---
+++ Week1/Week2/NumericTypes.cs
@@ -0,0 +1,44 @@
+namespace Week2
+{
+    public class NumericTypes
+    {
+        #region Integral - Signed
+        private sbyte _mySbyteValue = 2;
+
+        private short _myShortValue = 4;
+
+        private int _myIntValue = 25000;
+
+        private long _myLongValue = 1_234_567L;
+
+        private long _myLongValue2 = 0x5F;
+        #endregion
+    
+        #region Integral - Unsigned
+
+        private byte _myByteValue = 3;
+
+        private ushort _myUshortValue = 35;
+
+        private uint _myUInt = 2345U;
+
+        private ulong _myULong = 1234UL;
+        #endregion
+
+        #region Real
+
+        private float myFloatValue = -123.6589F;
+
+        private double _myDoubleValue = 12.34D;
+
+        private double _myDoubleValue2 = 1E06;
+
+        private double _myDoubleValue3 = 12.34;
+
+        private decimal _myDecimalValue =12.234M;
+
+        #endregion
+
+        
+    }
+}
