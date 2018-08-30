b2dd0c03e6b98acb723f90084f500facc1a3d1b5
paxkharybdis <35146073+paxkharybdis@users.noreply.github.com>
2018-08-30 04:09:01 -0500

    Week one homework

---
+++ Week1/W1 Homework/csharpcertw1hmwk/Bicycle.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace Week1.W1_Homework.csharpcertw1hmwk
+{
+
+        // Parent class
+    public class Bicycle
+    {
+        // Class variable: Field
+        private string _handlebar;
+      
+
+        // Class variable:  Auto Property
+        public virtual int Gears {get;} =7;
+
+        // Class variable: Property
+        public string Handlebar
+      {
+          set
+          {
+              this._handlebar = value;
+          }
+      }
+
+        // Class variable : Auto Property
+        public string Wheels { set; get; }
+
+
+        // Constructor
+        public Bicycle()
+        {
+            Console.WriteLine("Bicycle Default");
+
+        }
+
+        // Constructor with parameter
+        public Bicycle(string type)
+        {
+            Console.WriteLine("Bicycle with parameter");
+        }
+
+        // Method with parameter
+        public void Pedal(string type)
+        {
+            Console.WriteLine(type);
+        }
+
+
+
+    }
+}
