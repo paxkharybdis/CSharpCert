f325c3da8abf7e8bb41fb6a2bc2767348855043b
paxkharybdis <35146073+paxkharybdis@users.noreply.github.com>
2018-09-02 22:26:44 -0500

    Homework

---
+++ Week1/Week2/week2homework/gradeswitch.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Week1.Week2.week2homework
+{
+    public class gradeswitch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a grade.");
+            
+            string grade = Console.ReadLine();
+
+            switch(grade)
+            {
+                case "E":
+                case "e":
+                    Console.WriteLine("Excellent");
+                    break;
+                case "V":
+                case "v":
+                    Console.WriteLine("Very Good");
+                    break;
+                case "G":
+                case "g":
+                    Console.WriteLine("Good");
+                    break;
+                case "A":
+                case "a":
+                    Console.WriteLine("Average");
+                    break;
+                case "F":
+                case "f":
+                    Console.WriteLine("Fail");
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid grade.");
+                    break;
+            }
+        
+        }
+    }
+}
